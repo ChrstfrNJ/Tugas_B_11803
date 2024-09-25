45a0c0c9bda5550a9664ca408cee2ff3d649b2fe
ChrstfrNJ <220711803@students.uajy.ac.id>
2024-09-25 16:31:46 +0700

    Update Tugas_Modul 2_B_Christofer_11803.py

--- Tugas_Modul 2_B_Christofer_11803.py
+++ Tugas_Modul 2_B_Christofer_11803.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = "C:\Users\H P\OneDrive\Documents\mldl\Tugas_B_11803"
-model=os.path.join(model_path, 'GBT_heartDisease_model.pkl')
+model_path = "GBT_heartDisease_model.pkl"
 
 with open(model, 'rb') as f:
     loaded_model = pickle.load(f)
